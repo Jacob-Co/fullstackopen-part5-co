83f4c6892ddf9632eacaba87bd913fa0cb046bc9
Jacobtco <jacobtco@gmail.com>
2020-08-24 16:57:06 +0800

    Created login.js service and modified handleLogin to setUser depending on what is returned by POST /api/login

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+// Components
 import BlogList from './components/BlogList';
 import Login from './components/Login';
 
+// Server Request Helpers
 import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -17,9 +20,18 @@ const App = () => {
     );
   }, []);
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    console.log(username, password);
+    try {
+      const token = await loginService.login({ username, password });
+      setUser(token);
+      setUsername('');
+      setPassword('');
+    } catch (e) {
+      alert('Invalid username of password');
+      setUsername('');
+      setPassword('');
+    }
   };
 
   return (
