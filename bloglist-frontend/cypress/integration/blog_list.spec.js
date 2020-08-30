9c8bcb252829cccfb5e649dc7a87ad3d9518faaf
Jacobtco <jacobtco@gmail.com>
2020-08-30 10:01:59 +0800

    Added a cypress test for initial login page

--- bloglist-frontend/cypress/integration/blog_list.spec.js
+++ bloglist-frontend/cypress/integration/blog_list.spec.js
@@ -3,4 +3,11 @@ describe('Blog app', function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset');
     cy.visit('http://localhost:3000');
   });
+
+  it('should contain login page', function() {
+    cy.contains('Log in to Application');
+    cy.contains('username');
+    cy.contains('password');
+    cy.contains('Login');
+  });
 });
